a38dea168e68edff831321707c9f6737c2517650
Hitesh Dutt <hidutt@microsoft.com>
2024-10-23 15:04:58 +0530

    changes for making update issues sequential

--- src/business/update-sonar-issues-excel.ts
+++ src/business/update-sonar-issues-excel.ts
@@ -14,18 +14,19 @@ export class UpdateSonarIssuesExcel {
 
     async upload(): Promise<void> {
         console.time('UpdateSonarIssuesExcel');
-        const values = ExcelUtility.read(`${appsettings.branch}.xlsx`, 'Rules') as any[];
+        const workbookPath = `${appsettings.outputDirectory}/${Utility.getExcelFileNameFromBranch(appsettings.branch)}`;
+        const values = ExcelUtility.read(workbookPath, 'Rules') as any[];
         for (let i = 0; i < values.length; i++) {
             console.log(`Updating Rule ${values[i].sheetIdentifier} - Rule Count ${i + 1} / ${values.length}`);
             if (values[i].action === 'noaction') {
                 continue;
             }
-            const issuesKeys = ExcelUtility.read(`${appsettings.branch}.xlsx`, values[i].sheetIdentifier) as any[];
+            const issuesKeys = ExcelUtility.read(workbookPath, values[i].sheetIdentifier) as any[];
             const issuesKeysCsv = Utility.getIssuesInBlockOfNumber(issuesKeys, appsettings.maxRecordsToBeUpdaate);
-            issuesKeysCsv.forEach(async (element: any) => {
+            for(let element of issuesKeysCsv){
                 const response = await this.sonarWriteUpdateInfo.updateBulkIssuesToSonar(element, values[i].action);
                 console.log(`Response From Sonar : ${JSON.stringify(response)}`);
-            });
+            }
         }
         console.timeEnd('UpdateSonarIssuesExcel');
     }

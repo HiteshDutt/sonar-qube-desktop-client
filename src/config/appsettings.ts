c672c4c6b2225e6a98dc442c53d8221020aa36e8
nitingarg <nitingarg@microsoft.com>
2025-06-26 18:47:11 +0530

    Fix: Added logic for bulk update

--- src/config/appsettings.ts
+++ src/config/appsettings.ts
@@ -1,14 +1,14 @@
 export const appsettings = {
-    sonarBaseUrl: 'https://servicessonar.microsoft.com/',
-    sonarToken: 'squ_39524963f37a259500206e2a1f6f770cfb9deb4a',
-    sonarProjectKey: 'GlobalIT_R3C_DEV_NewProfile',
+    sonarBaseUrl: '',
+    sonarToken: '',
+    sonarProjectKey: '',
     langugage: ['cs','ts','web','tsql'], //cs,ts,web,plsql,tsql
     issueStatues:"ACCEPTED,OPEN",
-    branch: 'features/features-APT/feature-server-api-TB',
-    compareBranch: 'releases/release-5.2.1/release-5.2.1',
+    branch: '',
+    compareBranch: [],
     pageSize: 500,
     maxRecordsToBeUpdaate: 500,
-    outputDirectory: './output',
+    outputDirectory: '/output',
     parallelReadCalls: 100,
     requireParallelRuleRead: false
 }
